341cbc6956abcad4d3e13de86c845ba5b6ba86a8
Garance AR <79841946+TheWonderfulLizardOfOz@users.noreply.github.com>
2022-12-06 05:31:03 +0000

    Added Day 6

---
+++ Day 6/Day6.py
@@ -0,0 +1,53 @@
+def part1():
+    file = open("input.txt", "r")
+    data = file.readline()
+    file.close()
+
+    packets = ["", "", "", ""]
+    count = 0
+    for c in data:
+        count += 1
+
+        if count > 7:
+            share = False
+            for i in range(len(packets)):
+                if packets[i] in packets[i+1::]:
+                    share = True
+
+            if not share:
+                print(c)
+                return count - 1
+
+        packets.append(c)
+        packets.pop(0)
+
+    return -1
+
+def part2():
+    file = open("input.txt", "r")
+    data = file.readline()
+    file.close()
+
+    packets = [""]*14
+    print(packets)
+    count = 0
+    for c in data:
+        count += 1
+
+        if count > 7:
+            share = False
+            for i in range(len(packets)):
+                if packets[i] in packets[i + 1::]:
+                    share = True
+
+            if not share:
+                print(c)
+                return count - 1
+
+        packets.append(c)
+        packets.pop(0)
+
+    return -1
+
+print(part1())
+print(part2())
